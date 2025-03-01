b2d15bed8becf5ee7fdd6a3918b5ac79eab3139a
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-03-01 11:31:45 -0500

    Day 677: Adding Logic When Incrementing Variables

---
+++ Day_677.py
@@ -0,0 +1,22 @@
+
+
+
+numbers = [3,6,7,2,1]
+
+gte_5 = 0
+lt_5 = 0
+
+for num in numbers:
+    gte_5 += 1 if num >=5 else 0
+    lt_5 += 1 if num <5 else 0
+
+print(f'{gte_5=}, {lt_5=}')
+
+
+
+
+
+
+
+
+
