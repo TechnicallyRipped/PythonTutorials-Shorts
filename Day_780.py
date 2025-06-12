4bc01141ced9f4344a03108beb8f901709d6297a
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-06-12 00:53:05 -0400

    Day 780: Render HTML In Streamlit Web App

---
+++ Day_780.py
@@ -0,0 +1,11 @@
+
+
+import streamlit as st
+
+st.title("My App")
+
+st.markdown('''<h2 style='color:red'>
+                    Header 2
+                </h2>''',
+                unsafe_allow_html=True)
+
