19137e4a22319e03a6cda39ffe55ca3b7b0c3078
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-06-24 23:50:11 -0400

    Day 793: Fractional Operations In Python

---
+++ Day_793.py
@@ -0,0 +1,17 @@
+
+
+from fractions import Fraction
+
+x = Fraction(1,2)
+y = Fraction(1,4)
+
+print(x+y) # Addition
+
+print(x-y) # Subtraction
+
+print(x*y) # Multiplication
+
+print(x/y) # Division
+
+print(x>y)
+print(y>x)
