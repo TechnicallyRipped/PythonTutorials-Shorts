1f4c0f14790ccc6e5644e5162963fa07b613c5e8
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-06-30 21:31:08 -0400

    Day 799: Python Pop Quiz #1

---
+++ Day_799.py
@@ -0,0 +1,56 @@
+
+# Which is a mutable variable?  
+
+a = 10
+
+
+b = (1,2)
+
+
+c = [1,2,3]
+
+
+d = False
+
+
+
+
+
+# Which variable prints: 'Hello, World!'
+str1 = 'Hello'
+str2 = ','
+str3 = 'World!'
+
+a = print(str1,str2,str3)
+
+
+b = print(str1,str2,str3)
+
+
+c = print(str1,str2+str3)
+
+
+d = print(str1+str2,str3)
+
+
+
+
+
+
+# What is the output of my_list[1:3]
+my_list = [1,2,3,4,5]
+
+a = [1,2,3]
+
+
+b = [2,3]
+
+
+c = [2,3,4]
+
+
+d = [0,1,2]
+
+
+
+
