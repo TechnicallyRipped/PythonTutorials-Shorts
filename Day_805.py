3bab3092ca0f7ad3fb0ff4f193721444f0381aa4
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-07-07 10:56:13 -0400

    Day 805: The Easiest Way to Access Data with Dots in Python

---
+++ Day_805.py
@@ -0,0 +1,22 @@
+
+
+
+from types import SimpleNamespace
+
+person = SimpleNamespace(name='Joe',
+                          age=30)
+
+print(person.name)
+print(person.age)
+
+person.name = 'Chris'
+print(person.name)
+
+
+
+
+
+
+
+
+
