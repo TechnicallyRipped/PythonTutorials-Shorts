5fe8d576e13a57e0b860954fe2e74f4837e06b41
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-07-23 22:44:09 -0400

    Day 822: Saving Plotly Charts As Images

---
+++ Day_822.py
@@ -0,0 +1,13 @@
+
+
+import plotly.express as px
+
+x = ['A','B','C']
+y = [10,20,15]
+
+fig = px.bar(x=x,y=y)
+
+# fig.show()
+# fig.write_image("line_chart.png") 
+
+# python -m pip install kaleido
