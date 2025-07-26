27597e4f382a17a34816acfdfed803355c2e430b
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-07-26 09:57:12 -0400

    Day 824: How to Animate Charts with Plotly Express

---
+++ Day_824.py
@@ -0,0 +1,14 @@
+
+import plotly.express as px
+
+cats = ['A','A','A','A','B','B','B','B',]
+vals = [20,30,40,10,40,10,40,50]
+years = [2020,2021,2022,2023,
+         2020,2021,2022,2023]
+
+fig = px.bar(x=cats,y=vals,
+             color=cats,
+             range_y=[0, 60],
+             animation_frame=years)
+
+fig.show()
