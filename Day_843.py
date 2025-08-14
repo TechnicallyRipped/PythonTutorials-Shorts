99817d32e09783806b4ef6f97b35e508737ee5e1
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-08-13 22:58:49 -0400

    Day 843: Line Plots in Matplotlib and Plotly Side by Side

---
+++ Day_843.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+
+x = [0, 1, 2, 3, 4]
+y = [0, 1, 4, 9, 16]
+
+plt.plot(x, y)
+plt.title("Matplotlib Example")
+plt.show()
+
+
+# import plotly.express as px
+
+# fig = px.line(x=x, y=y, 
+#               title="Plotly Express Example")
+# fig.show()
