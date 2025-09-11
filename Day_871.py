ac58575324532f049f41a2ba5dc388a5044c72d8
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-09-10 22:45:45 -0400

    Day 871: Why Pylab Exists in Matplotlib

---
+++ Day_871.py
@@ -0,0 +1,15 @@
+
+
+from matplotlib.pylab import *
+
+x = [0, 1, 2, 3, 4, 5]
+y = [0, 1, 4, 9, 16, 25]
+
+
+plot(x, y)
+xlabel("X-axis") 
+ylabel("Y-axis") 
+title("Line plot of x vs y") 
+grid(True)
+
+show()
