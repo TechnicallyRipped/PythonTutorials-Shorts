ac1c27543c97b543e636f270e16889202fde738c
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-11-10 22:49:28 -0500

    Day 930: Python’s Walrus Operator Explained

---
+++ Day_930.py
@@ -0,0 +1,39 @@
+
+
+
+
+# x = int(input('Enter number > 5: '))
+
+# if x > 5:
+#     print('Success')
+# else:
+#     print('Fail')
+
+#----------------------------------------------
+
+if (x := int(input('Enter number > 5: '))) > 5:
+    print('Success')
+else:
+    print('Fail')
+
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
