48841abfadea6615dfe8c7160a9eb87dabee8cdc
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2025-12-08 11:48:23 -0500

    Day 957: Python Trick Find the Common Folders

---
+++ Day_957.py
@@ -0,0 +1,15 @@
+
+
+
+from os.path import commonpath
+
+file_1 = 'Documents/Code/Year/2025/Hello.txt'
+file_2 = 'Documents/Code/Year/2024/World.txt'
+file_3 = 'Documents/Code/Pics/img1.jpeg'
+
+print(commonpath([file_1,file_2,file_3]))
+
+
+
+
+
