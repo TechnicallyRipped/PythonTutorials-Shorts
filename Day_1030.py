223b1c14719d39d9bd47135bc0108bb0df7863e1
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-02-20 11:44:01 -0500

    Day 1030: How to Add Text to a Matplotlib Plot

---
+++ Day_1030.py
@@ -0,0 +1,17 @@
+
+
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4]
+y = [1,2,3,4]
+
+plt.plot(x,y)
+
+plt.text(2,3,'Hello World!',
+         fontsize=30,
+         color='red')
+
+plt.show()
+
+
+
