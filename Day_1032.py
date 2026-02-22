892f92efefb0663942fe22f3c3961c8fc9333eff
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-02-22 11:45:59 -0500

    Day 1032: The Matplotlib Trick Nobody Uses

---
+++ Day_1032.py
@@ -0,0 +1,25 @@
+
+
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4]
+y1 = [1,2,3,4]
+y2 = [4,3,2,1]
+
+plt.plot(x,y1,'red',x,y2,'green')
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# plt.plot(x,y1,'red',x,y2,'yellow')
