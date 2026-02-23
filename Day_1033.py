8c77ee4ac25a6abe7513488833ec3c8af9aa610f
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-02-23 08:12:37 -0500

    Day 1033: Write Better Matplotlib Code with This Small Change

---
+++ Day_1033.py
@@ -0,0 +1,26 @@
+
+
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4]
+y = [1,2,3,4]
+
+line, = plt.plot(x,y)
+
+line.set_color('green')
+line.set_linewidth(5)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# plt.plot(x,y1,'red',x,y2,'yellow')
