b5af78a2aa6cd3b0337556f7d06a12540e4dc20c
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-03-31 22:40:33 -0400

    Day 1064: Checking For Values In A List

---
+++ Day_1064.py
@@ -0,0 +1,14 @@
+
+
+
+my_list = ['a','b','c','d']
+
+val = 'a'
+
+if val in my_list:
+    print('Value in list')
+else:
+    print('Value not in list')
+
+result = True if val in my_list else False
+print(result)
