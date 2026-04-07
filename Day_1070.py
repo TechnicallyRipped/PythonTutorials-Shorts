b39831b5fc9561a44f5f518ae6af23d0fca9c4bd
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-04-06 22:29:15 -0400

    Day 1070: Print In Color With Python

---
+++ Day_1070.py
@@ -0,0 +1,9 @@
+
+
+
+from rich import print
+
+print("[bold blue]Hello World[/]")
+print("[italic green]Hello World[/]")
+print("[underline red]Hello World[/]")
+print("[red on green]Hello World[/]")
