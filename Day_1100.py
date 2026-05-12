4773c924921eda23c5ea95281373a0376ae571b8
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-05-11 21:24:44 -0400

    Day 1100: Python Integer Operations

---
+++ Day_1100.py
@@ -0,0 +1,31 @@
+
+
+
+
+x = 25
+y = 4
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+exponent = x ** y
+floor_division = x // y
+remainder = x % y
+
+
+
+
+print(f"{addition=}")
+print(f"{subtraction=}")
+print(f"{multiplication=}")
+print(f"{division=}")
+print(f"{exponent=}")
+print(f"{floor_division=}")
+print(f"{remainder=}")
+
+
+
+
+
+
