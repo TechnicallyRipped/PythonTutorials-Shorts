536bf8992ee59c41de5ee2b09c819de97e4f3719
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-06-08 23:52:37 -0400

    Day 1125: How to Refresh a Webpage with Selenium

---
+++ Day_1125.py
@@ -0,0 +1,20 @@
+
+
+
+from selenium.webdriver import Chrome
+import time
+
+driver = Chrome()
+
+driver.get('https://technicallyripped.com')
+
+time.sleep(1)
+driver.refresh()
+
+time.sleep(1)
+driver.refresh()
+
+time.sleep(1)
+
+
+
