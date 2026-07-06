8178e549e02f832c0cf64999463e68052ac20441
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-07-06 08:12:21 -0400

    Day 1148: How to Clear the Clipboard With Python

---
+++ Day_1148.py
@@ -0,0 +1,15 @@
+
+
+
+import win32clipboard
+
+win32clipboard.OpenClipboard()
+win32clipboard.EmptyClipboard()
+win32clipboard.CloseClipboard()
+
+import pyperclip
+
+pyperclip.copy("")
+
+# My clipboard text 
+# 
