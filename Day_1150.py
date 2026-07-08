21936b899b5e99cfc67d1d79d56990113a101047
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-07-07 22:36:32 -0400

    Day 1150: Python List Reverse Leetcode Problem

---
+++ Day_1150.py
@@ -0,0 +1,14 @@
+
+
+
+# Reverse the values in a list
+# without altering the original order
+
+ogList = ['abc','def','ghi']
+
+# result = ['cba','fed','ihg']
+
+result = [i[::-1] for i in ogList]
+
+print(result)
+
