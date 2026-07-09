393d906a44b8d0fe2bc964f8824b90578a32502f
TechnicallyRipped <156479004+TechnicallyRipped@users.noreply.github.com>
2026-07-08 21:14:52 -0400

    Day 1151: Python List Quiz

---
+++ Day_1151.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+x = [1,2,3]
+y = [3,4,5]
+
+# 1: Which list operation 
+#    doesn't exist?
+
+# A: x + y
+# B: x - y
+# C: x * 3
+# D: y * 2
+
+
+
+
+
+
+
+
+
+x = [1,2,3]
+
+# 2. Which list method does exist?
+
+# A: x.cache()
+# B: x.delete()
+# C: x.duplicate()
+# D: x.clear()
+
+
+
+
+
+
+
+
+
+
+z = [[1,2,3],[4,5,6]]
+
+# 3. Which is equal to 
+#    the value 2?
+
+# A: z[0]
+# B: z[1]
+# C: z[0][0]
+# D: z[0][1]
